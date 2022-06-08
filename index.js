1b70994bb52ba24e1abd5f3f42c23a3fb2eed74e
LuizGrochevski <luizfelipegrochevski@gmail.com>
2022-06-08 18:21:27 -0300

    m

---
+++ index.js
@@ -0,0 +1,6 @@
+var numero = Number(window.prompt("digite um numero:"))
+var numero2 = Number(window.prompt("Digite outro numero:"))
+
+var soma = numero + numero2
+
+window.alert(`Os Valores somados são ${soma}`)
